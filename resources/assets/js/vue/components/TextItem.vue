17027510f4ad7afd43fbddb0bcc5d221b40b5584
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-07-09 02:33:05 +0300

    1px

--- resources/assets/js/vue/components/TextItem.vue
+++ resources/assets/js/vue/components/TextItem.vue
@@ -92,6 +92,7 @@ export default {
     position: relative;
     padding: 0 !important;
     margin: 0 !important;
+    cursor: text;
 }
 
 .clear-input {
@@ -124,6 +125,7 @@ export default {
     font-weight: normal;
     color: #000000;
     background: white;
+    cursor: text;
 }
 
 [contenteditable=true]:hover {
