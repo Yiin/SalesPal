b7e7177af0a8fcc1f814f8345765bb890a8a0bc8
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-07-16 04:54:28 +0300

    client create/edit, table state fix, vat checker in progress

---
+++ resources/assets/js/vue/components/partials/ClientVatChecker.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="vat-clients-wrapper">
+        <div class="vat-wrapper">
+            <div class="vat-panel-header">
+                Check Your Vat Number
+                <hr>
+            </div>
+            <div class="vat-panel-body">
+                <div class="vat-panel-body-header">
+                    <span>Vat Number <input type="text" value=""><br></span>
+                    <div class="btn-primary btn check-button">
+                        Check
+                    </div>
+                    <hr>
+                </div>
+                <div v-for="result in results" class="vat-result">
+                    <input v-if="result.just_checked" type="hidden" name="vat_checks[][is_valid]" :value="result.is_valid ? 1 : 0">
+                    <div :class="{ 'red-border': !result.is_valid }" class="border"></div>
+                    <div class="details">
+                        <div class="detail">
+                            VAT: <span>{{ result.country_code + result.vat_number }}</span>
+                        </div>
+                        <div :class="[ result.is_valid ? 'green-text' : 'red-text' ]" class="detail">
+                            Status: <span>{{ result.is_valid ? 'Correct' : 'Incorrect' }}</span>
+                        </div>
+                        <div class="detail">
+                            Checked at: <span>{{ result.created_at }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'vatChecks'
+        ],
+
+    }
+</script>
---
+++ resources/assets/js/vue/components/partials/ContactsPanel.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="form-wrapper">
+        <div class="contacts-panel-header new-clients-header">
+            Contacts
+        </div>
+        <div class="form-panel-body">
+            <div v-for="(form, index) in forms" class="contact-form">
+                <hr v-if="index">
+                <div class="flex-grid">
+                    <div class="col">
+                        <span>First Name <input :name.once="`contacts[${index}][first_name]`" type="text" v-model="form.first_name"><br></span>
+                    </div>
+                    <div class="col">
+                        <span>Last Name <input :name.once="`contacts[${index}][last_name]`" type="text" v-model="form.last_name"><br></span>
+                    </div>
+                    <div class="col item">
+                        <span>Job Position <input readonly type="text" v-model="form.job_position"><br></span>
+                    </div>
+                </div>
+                <div class="flex-grid">
+                    <div class="col">
+                        <span>Email <input :name.once="`contacts[${index}][email]`" type="text" v-model="form.email"><br></span>
+                    </div>
+                    <div class="col">
+                        <span>Phone <input :name.once="`contacts[${index}][phone]`" type="text" v-model="form.phone"><br></span>
+                    </div>
+                </div>
+            </div>
+            <div class="new-client-link">
+                <a @click.prevent="add">+ Add New Contact</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'contacts'
+        ],
+        data() {
+            let forms = [];
+
+            if (this.contacts) {
+                this.contacts.forEach(contact => {
+                    forms.push({
+                        first_name: contact.first_name,
+                        last_name: contact.last_name,
+                        email: contact.email,
+                        phone: contact.phone
+                    });
+                });
+            }
+            else {
+                forms.push({});
+            }
+
+            return {
+                forms
+            };
+        },
+        methods: {
+            add() {
+                this.forms.push({});
+            }
+        }
+    }
+</script>
+
+<style>
+    input[readonly] {
+        cursor: no-drop;
+    }
+</style>
